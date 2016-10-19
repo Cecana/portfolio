1e308b5c914e4b7ddb11f700f62ad01cb20e3a82
Cecana <cecanam@gmail.com>
2016-10-19 22:09:35 +0200

    Replace link to the quiz

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ function portfolioComponent ({ data = {}, theme = {} } = {}) {
   }
   function unhoverPortfolio() { yo.update(element, template()) }
   function openNewTab() {
-    var url = "https://esova-ana.github.io/quiz/"
+    var url = "https://cecana.github.io/quiz/"
     var tab = window.open(url, '_blank')
     tab.focus()
   }
